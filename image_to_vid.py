9f45f403759f12fc85dcbbb9b0d63b4ab7c99bdc
boofydon <fatoki103@gmail.com>
2020-10-13 21:07:45 -0700

    first commit

---
+++ image_to_vid.py
@@ -0,0 +1,14 @@
+from tkinter import * #The * indicates it imports everything in tkinter
+from tkinter.font import Font
+from tkinter import filedialog
+from copy_cut_func import rClicker
+from copy_cut_func import rClickbinder
+
+root =Tk()
+
+root.title("Image merger") #Set title
+root.geometry('1024x768+500+100') #set size of window and location it appears
+root.resizable(width=True, height=True)#Prevent user from changing size of window
+rClickbinder(root)#Enable copy paste
+
+root.mainloop()
