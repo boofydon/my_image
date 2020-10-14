72f35bcf67eaa00e07bfbfd3daa80a44f3f69cf0
boofydon <fatoki103@gmail.com>
2020-10-13 21:19:37 -0700

    Added more comment

--- image_to_vid.py
+++ image_to_vid.py
@@ -9,6 +9,6 @@ root =Tk()
 root.title("Image merger") #Set title
 root.geometry('1024x768+500+100') #set size of window and location it appears
 root.resizable(width=True, height=True)#Prevent user from changing size of window
-rClickbinder(root)#Enable copy paste
+rClickbinder(root)#Enable copy paste as long
 
 root.mainloop()
